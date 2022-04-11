27884c77690e19b7abfef059d1c67e6be8492887
jgw <guowen.jin@fit2cloud.com>
2022-04-11 14:45:58 +0800

    fix:catalog记录修改

--- rackshift-server/src/main/java/io/rackshift/engine/service/ZDHCatalogService.java
+++ rackshift-server/src/main/java/io/rackshift/engine/service/ZDHCatalogService.java
@@ -3,6 +3,7 @@ package io.rackshift.engine.service;
 import com.alibaba.fastjson.JSONObject;
 import io.rackshift.mybatis.domain.BareMetal;
 import io.rackshift.mybatis.domain.Catalog;
+import io.rackshift.mybatis.domain.CatalogExample;
 import io.rackshift.mybatis.mapper.CatalogMapper;
 import io.rackshift.service.BareMetalService;
 import io.rackshift.service.ProfileService;
@@ -12,6 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class ZDHCatalogService {
@@ -25,6 +27,11 @@ public class ZDHCatalogService {
         catalog.setSource(result.getString("source").trim());
         catalog.setData(result.getString("data"));
         catalog.setCreateTime(System.currentTimeMillis());
+        CatalogExample catalogExample = new CatalogExample();
+        CatalogExample.Criteria criteria = catalogExample.createCriteria();
+        criteria.andBareMetalIdEqualTo(bareMetalId).andSourceEqualTo(result.getString("source").trim());
+        //删除旧的数据，否则后续多raid磁盘查询处理修改麻烦
+        catalogMapper.deleteByExample(catalogExample);
         catalogMapper.insert(catalog);
     }
 }
